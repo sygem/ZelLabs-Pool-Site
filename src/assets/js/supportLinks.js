04f77ed7f6bb11d1fc01c0d9b5ddb772975d646a
SyGem Software <simon@sygem.com>
2021-03-29 13:55:45 +0100

    Initial version v0.3.3
    Includes loyalty program

---
+++ src/assets/js/supportLinks.js
@@ -0,0 +1,17 @@
+let fomoSupportText = "FluxLabs";
+let fomoSupportIcons = ["zel-icon","zel-icon-info-circle"];
+let fomoLinks = [{title:"FluxLabs Discord",link:"https://discord.gg/e8sFxpp",icon:["text-muted zel-icon","zel-icon-discord"]}];
+
+let zelSupportText = "Flux";
+let zelSupportIcons = ["zel-icon","zel-icon-info-circle"];
+let zelLinks = [{title:"Flux Discord",link:"https://discord.gg/svDHPud",icon:["text-muted zel-icon","zel-icon-discord"]},
+                 {title:"Flux Homepage",link:"https://zel.cash",icon:["zel-icon","zel-icon-home"]},
+                 {title:"Flux Explorer",link:"https://explorer.zel.cash",icon:["zel-icon","zel-icon-stack"]},
+                 {title:"Bitcointalk ANN",link:"https://bitcointalk.org/index.php?topic=2853688.0",icon:["zel-icon","zel-icon-bullhorn"]},
+                 {title:"Reddit",link:"https://www.reddit.com/r/ZelCash/",icon:["zel-icon","zel-icon-reddit-alien"]},
+                 {title:"Twitter",link:"https://twitter.com/Zelcash",icon:["zel-icon","zel-icon-twitter"]},
+                 {title:"GitHub",link:"https://github.com/zelcash",icon:["zel-icon","zel-icon-github-alt"]},
+                 {title:"CoinMarketCap",link:"https://coinmarketcap.com/currencies/zelcash/",icon:["zel-icon","zel-icon-pie-chart"]},
+                 {title:"Coin Gecko",link:"https://www.coingecko.com/en/coins/zelcash",icon:["zel-icon","zel-icon-pie-chart"]},
+                 {title:"Coin 360",link:"https://coin360.com/coin/zelcash-zel",icon:["zel-icon","zel-icon-pie-chart"]},
+                ];
